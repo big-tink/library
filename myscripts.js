9e9d067159b66f18613cebbfb9ad00defc80acb5
Jake Bellm <[jbsnake13@gmail.com]>
2024-09-20 23:48:13 -0500

    Make function to loop array and display books

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,30 @@
-const myLibrary = []
+const myLibrary = [
+    {title: 'The Hobbit'},
+    {title: 'The Hunger Games'},
+    {title: 'I did it, im the best no ones better ever anywhere'}
+]
 
 function Book() {
 
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+function displayBooks(library) {
+
+    library.forEach(function(books) {
+        let bookTitle = books.title
+        const row = document.querySelector('.row1')
+        const book = document.createElement('div')
+
+        book.setAttribute('class', `book`)
+        book.style.backgroundColor = 'rgb(0, 0, 0)'
+        book.textContent = bookTitle;
+
+        row.appendChild(book)
+    })
+}
+
+displayBooks(myLibrary);
